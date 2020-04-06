705f153603f7d2969f6080dbb4477fd77fe9a2c3
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-05 23:06:49 -0500

    setup the frame for the dashboard with 3 statuses

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,22 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavHelperService } from "src/app/services/nav-helper.service";
 import { Application } from "src/app/models/Application.model";
 import { ApplicationService } from "src/app/services/application.service";
 import { CookieHelper } from "src/app/utilities/cookie.util";
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
 
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.css"]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public applications: Application[] = null;
+
+  public get ready(): boolean {
+    return BooleanHelper.hasValue(this.applications);
+  }
 
   public get featuredApps(): Application[] {
-    return this.appService.featuredApps;
+    return this.applications.filter((app) => {
+      return app.status === 2;
+    });
+  }
+
+  public get readyApps(): Application[] {
+    return this.applications.filter((app) => {
+      return app.status === 1;
+    });
   }
 
   public get archivedApps(): Application[] {
-    return this.appService.archivedApps;
+    return this.applications.filter((app) => {
+      return app.status === 0;
+    });
   }
 
   public get showArchive(): boolean {
@@ -28,6 +44,10 @@ export class DashboardComponent {
     private appService: ApplicationService,
   ) { }
 
+  public ngOnInit() {
+    this.loadApps();
+  }
+
   public goToInfo() {
     this.navHelper.goToInfo();
   }
@@ -35,5 +55,15 @@ export class DashboardComponent {
   public goToDonate() {
     this.navHelper.goToDonate();
   }
+
+  private loadApps(): void {
+    this.applications = null;
+    this.appService.getApplications()
+      .subscribe((res) => this.applications = res,
+        (error) => {
+          console.log("get apps failed");
+        });
+  }
+
 
 }
