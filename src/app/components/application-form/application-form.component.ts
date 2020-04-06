5481c4ed3bb66a3b9ccb7588f9e780da497ecdef
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-05 22:37:23 -0500

    create works

--- src/app/components/application-form/application-form.component.ts
+++ src/app/components/application-form/application-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Application } from 'src/app/models/Application.model';
+import { BooleanHelper } from 'src/app/utilities/boolean.util';
+import { ActivatedRoute } from '@angular/router';
+import { ApplicationService } from 'src/app/services/application.service';
+import { NavHelperService } from 'src/app/services/nav-helper.service';
 
 @Component({
   selector: 'app-application-form',
@@ -6,10 +11,117 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application-form.component.scss']
 })
 export class ApplicationFormComponent implements OnInit {
+  public application: Application = null;
+  public showErrors = false;
 
-  constructor() { }
+  public get title(): string {
+    if (this.editMode) {
+      return "Edit Application";
+    }
+    return "Create Application";
+  }
 
-  ngOnInit() {
+  public get errors(): string[] {
+    const myErrors = [];
+    if (this.nameInvalid) {
+      myErrors.push("Please provide a name.");
+    }
+    return myErrors;
+  }
+
+  public get valid(): boolean {
+    return this.errors.length === 0;
+  }
+
+  public get ready(): boolean {
+    return BooleanHelper.hasValue(this.application);
+  }
+
+  public get editMode(): boolean {
+    return this.ready && BooleanHelper.hasValue(this.application._id);
+  }
+
+  private get nameInvalid(): boolean {
+    return !BooleanHelper.hasValue(this.application.name);
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private applicationService: ApplicationService,
+    private navHelper: NavHelperService,
+  ) { }
+
+  public ngOnInit() {
+    this.setupForm();
+  }
+
+  public updateDescription(event) {
+    this.application.description = event.target.value;
+  }
+
+  public submit() {
+    this.showErrors = true;
+    if (this.valid) {
+      this.save();
+    }
+  }
+
+  private save() {
+    if (this.editMode) {
+      this.runEdit();
+    } else {
+      this.runCreate();
+    }
+  }
+
+  private runEdit() {
+    let response;
+    this.applicationService.updateApplication(this.application)
+      .subscribe((res) => response = res,
+        (error) => {
+          console.log("update application failed");
+        }, () => {
+          this.navHelper.goToApplicationOverview();
+        });
+  }
+
+  private runCreate() {
+    let response;
+    this.applicationService.createApplication(this.application)
+      .subscribe((res) => response = res,
+        (error) => {
+          console.log("create application failed");
+        }, () => {
+          this.navHelper.goToApplicationOverview();
+        });
+  }
+
+  private setupForm() {
+    const urlId = this.route.snapshot.paramMap.get("id");
+    if (BooleanHelper.notNull(urlId)) {
+      this.setupEditForm(urlId);
+    } else {
+      this.setupCreateForm();
+    }
+  }
+
+  private setupEditForm(id: string): void {
+    this.applicationService.getSingleApplication(id)
+      .subscribe((res) => this.application = res,
+        (error) => {
+          console.log("get application failed");
+        });
+  }
+
+  private setupCreateForm(): void {
+    this.application = {
+      name: null,
+      description: null,
+      link: null,
+      icon: null,
+      status: 0,
+      _id: null,
+    };
   }
 
 }
