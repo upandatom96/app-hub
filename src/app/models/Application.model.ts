4337f1d274a7126254025ca991a96030bb2fe832
Adam Callanan <a.callanan@sbcglobal.net>
2020-04-05 22:06:43 -0500

    reworking app model and starting on service

--- src/app/models/Application.model.ts
+++ src/app/models/Application.model.ts
@@ -3,5 +3,5 @@ export interface Application {
   link: string;
   description: string;
   icon: string;
-  archived: boolean;
+  status: number;
 }
--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Application } from "../models/Application.model";
 import { APPLICATIONS } from "../constants/application.constants";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RestUrlBuilder } from '../utilities/rest-url-builder.util';
+import { ServiceUrl } from '../constants/rest.constants';
+import { CookieHelper } from '../utilities/cookie.util';
+
+const controller = "application";
 
 @Injectable({
   providedIn: "root"
@@ -12,13 +19,66 @@ export class ApplicationService {
 
   public get featuredApps(): Application[] {
     return this.allApps.filter((app) => {
-      return !app.archived;
+      return app.status === 1;
+    });
+  }
+
+  public get otherApplications(): Application[] {
+    return this.allApps.filter((app) => {
+      return app.status === 2;
     });
   }
 
   public get archivedApps(): Application[] {
     return this.allApps.filter((app) => {
-      return app.archived;
+      return app.status === 3;
     });
   }
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getApplication(): Observable<Application[]> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.get(url, CookieHelper.authHeaders) as Observable<Application[]>;
+  }
+
+  public getSingleApplication(applicationId: string): Observable<Application> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: applicationId,
+    });
+    return this.http.get(url, CookieHelper.authHeaders) as Observable<Application>;
+  }
+
+  public createApplication(application: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.post(url, application, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public updateApplication(application: any): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller
+    });
+    return this.http.put(url, application, CookieHelper.authHeaders) as Observable<any>;
+  }
+
+  public deleteApplication(applicationId: string): Observable<any> {
+    const url = RestUrlBuilder.buildRestUrl({
+      service: ServiceUrl.BasicExpress,
+      controller,
+      params: applicationId,
+    });
+    return this.http.delete(url, CookieHelper.authHeaders) as Observable<any>;
+  }
+
 }
