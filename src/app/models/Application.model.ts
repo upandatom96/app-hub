6b8892af93c3c97f28ddfd82b78f3fe333c3d4b3
Adam Callanan <a.callanan@sbcglobal.net>
2020-05-28 19:07:50 -0500

    update app model

--- src/app/models/Application.model.ts
+++ src/app/models/Application.model.ts
@@ -1,6 +1,9 @@
 export interface Application {
   _id?: string;
   name: string;
+  tagline: string;
+  tech: string[];
+  githubLink?: string;
   link: string;
   description: string;
   icon: string;
