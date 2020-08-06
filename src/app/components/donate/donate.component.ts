b2c307c0b5ffa32e101421eb503d0d58eb70c576
Adam Callanan <a.callanan@sbcglobal.net>
2020-08-05 20:26:48 -0500

    add to donate

--- src/app/components/donate/donate.component.ts
+++ src/app/components/donate/donate.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import {Donation} from "../../models/Donation.model";
 
 @Component({
   selector: "app-donate",
@@ -6,4 +7,36 @@ import { Component } from "@angular/core";
   styleUrls: ["./donate.component.scss"]
 })
 export class DonateComponent {
+  public donations: Donation[] = [
+    {
+      name: "Black Lives Matter",
+      details: "Donate (and more) for Black Lives Matter.",
+      link: "https://blacklivesmatters.carrd.co/",
+    },
+    {
+      name: "Homeless Black Trans Women Matter",
+      details: "Donate to help Homeless Black Trans Women.",
+      link: "https://www.gofundme.com/f/homeless-black-trans-women-fund?utm_source=twitter&utm_medium=social&utm_campaign=p_cp+share-sheet",
+    },
+    {
+      name: "Black Hills Legal Fund",
+      details: "Support Indigenous People.",
+      link: "https://bhlegalfund.org/",
+    },
+    {
+      name: "RAICES",
+      details: "Donate to help fight for immigrant and refugee rights.",
+      link: "https://www.raicestexas.org/ways-to-give/donate/",
+    },
+    {
+      name: "Peter and Paul Community Services",
+      details: "Donate to help the homeless in St. Louis, Missouri.",
+      link: "https://ppcsinc.org/give/make-a-donation/",
+    },
+    {
+      name: "Meals on Wheels",
+      details: "Donate to help feed America.",
+      link: "https://ams.mealsonwheelsamerica.org/eweb/DynamicPage.aspx?WebCode=globaldonate&site=mowa&CampCode=AnnualCampaign&AplCode=AnnCampWeb&_ga=2.86054296.1679140158.1542118140-1937589897.1533304096",
+    },
+  ];
 }
---
+++ src/app/models/Donation.model.ts
@@ -0,0 +1,5 @@
+export interface Donation {
+  name: string;
+  link: string;
+  details: string;
+}
